3a22f3688db505d73b58fe2964ffc8c644cf430b
Madison LeBlanc <mleblanc8138@gmail.com>
2021-11-11 15:20:26 -0600

    Updated Header and About Page

--- mcBackend/mcReact/src/Pages/About/About.js
+++ mcBackend/mcReact/src/Pages/About/About.js
@@ -1,13 +1,45 @@
 import './About.css';
+import TextBox from '../../Components/TextBox/TextBox';
 
 function About() {
+
+  const aboutUsBox = <div>
+      About Us!
+
+    <br/>
+    </div>
+
+
+  const creatorsBox = <div>
+       Creators:
+       <ul>
+              <li>Ben Cassel</li>
+              <li>Allen Mire</li>
+              <li>Max Cole</li>
+              <li>Hayden Israel</li>
+              <li>Kyle Freeman</li>
+              <li>Madison LeBlanc</li>
+        </ul>
+      <br/>
+      
+      </div>;
+  const contactBox = <div>
+      Contact Info:
+      Client@email.com
+      Creator@email.com
+      <br/>
+     </div>;
+
+
   return (
     <div className="Main">
-      <div className="BodyMain">
-        <p>This is the body of About.</p>
+      <div className="AboutPage">
+      <TextBox className="about" text={aboutUsBox}/>
+      <TextBox className="create" text={creatorsBox}/>
+        <TextBox className="contact" text={contactBox} /> 
       </div>
 
-      {/* <Loading/> */}
+     
     </div>
   );
 }
